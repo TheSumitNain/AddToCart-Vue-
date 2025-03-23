c2da016354d95fe9335a59ebe691c0b475bebdef
sumit <nainsumit740@gmail.com>
2025-03-23 15:15:14 +0530

    update

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,17 +1,11 @@
 <template>
   <div>
     <Header />
-    <!-- <Cart/> -->
   <ProductList />
   </div>
 </template>
 
 <script setup>
-  import Cart from './Cart.vue';
 import ProductList from './ProductList.vue';
 import Header from './Header.vue';
 </script>
-
-<style lang="scss" scoped>
-
-</style>
